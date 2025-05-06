98ff13626db391fde3ea5baf9103e16a400eb5d3
Annalisa <annalisaochner@gmail.com>
2025-05-05 19:16:46 -0500

    update code

--- UserPathingCreatorTest/obj/Debug/net8.0-windows/UserPathingCreatorTest.AssemblyInfo.cs
+++ UserPathingCreatorTest/obj/Debug/net8.0-windows/UserPathingCreatorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserPathingCreatorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a8bc6897edca5af6382611bb7345d582c1e493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279d5f0e20b30dc3549d23bdf9b73ead5acda519")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserPathingCreatorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserPathingCreatorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
